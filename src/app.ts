c5680e04e2aeb3f65a6a88177fb60d37b244835f
esterfrazao <tafnisbonbon@gmail.com>
2022-09-20 13:06:57 -0300

    feat: add error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
+import "express-async-errors";
 import cors from "cors";
 import express from "express";
-import "express-async-errors";
+import handleAppError from "./middlewares/handleAppError.mdw";
 import router from "./routes";
 
 const app = express();
@@ -8,5 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
+app.use(handleAppError);
 
 export default app;
